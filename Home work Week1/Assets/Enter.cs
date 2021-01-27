4e364a1816517127c0e408a935f986ccb2e43227
DESKTOP-TGC6UDJ\RYU <chavin.pean@bumail.net>
2021-01-27 22:12:49 +0700

    Send Assignment Week 1

---
+++ Home work Week1/Assets/Enter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enter : MonoBehaviour
+{
+    public string findWord;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void Names()
+    {
+
+        findWord = inputField.GetComponent<Text>().text;
+
+        if (findWord == "Unity")
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "is found";
+        }
+        else if (findWord == "Unreal")
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "is found";
+        }
+        else if (findWord == "ResidentEvil")
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "is found";
+        }
+        else if (findWord == "Google")
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "is found";
+        }
+        else if (findWord == "MongoDB")
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "is found";
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = " [ " + findWord + " ] " + "not found";
+        }
+
+
+    }
+
+}
